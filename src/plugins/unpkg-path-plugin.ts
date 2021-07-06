239350cf1858b21f7fe2a82171881507c030686c
Thomas Boittin <tboittin@gmail.com>
2021-07-06 11:03:54 +0200

    71. Dynamically Fetching Modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,17 @@
-import * as esbuild from 'esbuild-wasm';
- 
+import axios from 'axios';
+import * as esbuild from 'esbuild-wasm'; 
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg') {
+          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
+        }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,11 +21,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('tiny-test-pkg')
               console.log(message);
             `,
           };
-        } 
+        }
+        
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data
+        }
       });
     },
   };
